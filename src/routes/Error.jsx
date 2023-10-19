077e13ad9b47ed1dbbd9d2b114a8fc921920cee4
arthur <arthurpianbessa@hotmail.com>
2023-10-19 07:41:05 -0300

    Atualizei o projeto com as alterações de responsividade

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,11 +1,16 @@
 import {} from 'react'
-
+import '../estilos/Erro.css'
+import Nav from "../components/Nav"
 
 function Error (){
   return(
-    <section >
-    <h1>404-Página não encontrada.</h1>
+    <>
+    <Nav />
+    <section className='conteudo_erro'>
+    <h1 className='erro'>404</h1>
+    <h1 className='sub-erro'>Página não encontrada.</h1>
     </section>
+    </>
   )
 }
 export default Error
